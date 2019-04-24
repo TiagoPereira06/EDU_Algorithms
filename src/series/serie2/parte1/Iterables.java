faac610a3b00dd77407eb2776a895b82b6f901c6
TiagoPereira06 <tiagomrp06@gmail.com>
2019-04-24 19:30:35 +0100

    project structure update

--- src/series/serie2/parte1/Iterables.java
+++
@@ -1,4 +0,0 @@
-package series.serie2.parte1;
-
-public class Iterables {
-}
--- src/series/serie2/parte1/ListUtils.java
+++ src/series/serie2/parte1/ListUtils.java
@@ -1,24 +1,10 @@
 package series.serie2.parte1;
-
 import java.util.Comparator;
 
 public class ListUtils {
 
-    private static class Node<E> {
-        public Node<E> prev, next;
-        public E value;
-
-        public Node() {
-            next = prev = this;
-        }
-        public Node(E k) {
-            value = k;
-        }
-    }
-
-    public static <E>Node<E>getMiddle(Node<E> list){
-
-        Node<E> fastJumper=list,slowJumper=list;
+   public static <E> Node<E> getMiddle(Node<E> list){
+       Node<E> fastJumper=list,slowJumper=list;
 
         while(fastJumper!=null && fastJumper.next!=null){
             fastJumper=fastJumper.next.next;
@@ -27,19 +13,16 @@ public class ListUtils {
 
         return slowJumper;
     }
-        /*Pior - O(n^2)
-        Médio - O(nlogn)
-        Melhor - O(nlogn)*/
 
-    <E> void quicksort(Node<E> first, Node<E> last, Comparator<E> cmp){
-
-      while (cmp.compare(first.value,last.value)<0){
-          //Node<E> q = partition()
-
-      }
-
-
-
-
+    public static <E>void quicksort(Node<E> first, Node<E> last, Comparator<E> cmp){
+        throw new UnsupportedOperationException();
     }
+
+
+
+    public static <E> Node<E> merge(Node<E>[] lists, Comparator<E> cmp){
+        throw new UnsupportedOperationException();
+    }
+
+
 }
